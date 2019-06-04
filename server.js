80e726e036b4694ad20f07bbfb0b6843505a4d2b
Gabriel Kirsten Menezes <gabriel.kirsten@hotmail.com>
2019-06-04 16:00:20 -0300

    Added require-dir

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const requireDir = require('require-dir')
 
 const app = express();
 
@@ -7,6 +8,8 @@ mongoose.connect(
     'mongodb://localhost:27017/nodepi',
     { useNewUrlParser: true },
 );
+
+requireDir("./src/models");
 
 app.get('/', (req, res) => {
     res.send("hello world!");
