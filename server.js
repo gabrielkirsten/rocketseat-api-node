b44ac9b9f0c8791bff4af25beb35b06f5f9f2b6a
Gabriel Kirsten Menezes <gabriel.kirsten@hotmail.com>
2019-06-04 15:58:39 -0300

    Added mongoose and first entity

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect(
+    'mongodb://localhost:27017/nodepi',
+    { useNewUrlParser: true },
+);
 
 app.get('/', (req, res) => {
     res.send("hello world!");
