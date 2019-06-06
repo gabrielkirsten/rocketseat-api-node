1d752c852c6ffa04b2fbcecf344c10337cf2b949
Gabriel Kirsten Menezes <gabriel.kirsten@hotmail.com>
2019-06-06 15:57:39 -0300

    Added crud methods

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,13 +4,35 @@ const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res) {
+        console.log('oi1');
         const products = await Product.find();
-
+        console.log('oi');
+        
         return res.json(products);
     },
-    async store(req, res) {
+
+    async show(req, res) {
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+
+    async create(req, res) {
         const product = await Product.create(req.body);
 
         return res.json(product);
-    }
+    },
+
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+        return res.json(product);
+    },
+
+    async destroy(req, res) {
+        const product = await Product.findByIdAndDelete(req.params.id);
+
+        return res.send();
+    },
+
 };
